dfb9b2154846f50c9be87dce193f7e735538fb79
VictorMonte <victorbcm@hotmail.com>
2017-09-27 19:02:04 -0300

    Add bootstrap server

--- server.go
+++ server.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
